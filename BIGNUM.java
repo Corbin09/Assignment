e5658c719b4f78d9151c629e236f01648ac8d9c1
Corbin09 <118120616+Corbin09@users.noreply.github.com>
2024-04-11 17:55:14 +0700

    Update BIGNUM.java

--- BIGNUM.java
+++ BIGNUM.java
@@ -1,9 +1,9 @@
 import java.util.*;
 import java.lang.*;
 
-class BIGNUM
+class BigNum
 {
-    public static int convertToInt(char c) {
+    public static int Int(char c) {
         if (c >= '0' && c <= '9') {
             return c - '0';
         } else {
@@ -16,26 +16,7 @@ class BIGNUM
         Scanner inp = new Scanner(System.in);
         String a = inp.nextLine();
         String b = inp.nextLine();
-        String big = "";
-        String small = "";
 
-        if(a.length() < b.length()){
-            big = b;
-            small = a;
-        } else if(b.length() < a.length()) {
-            big = a;
-            small = b;
-        } else {
-            for(int i = 0; i < a.length(); i++){
-                int x = convertToInt(a.charAt(i));
-                int y = convertToInt(b.charAt(i));
-                if(x > y){
-                    big = a;
-                    small = b;
-                    break;
-                }
-            }
-        }
         while (true){
             if (a.length() == b.length()) break;
             if(a.length() < b.length()){
@@ -45,5 +26,70 @@ class BIGNUM
                 b = "0" + b;
             }
         }
+        System.out.println(a);
+        System.out.println(b);
+//        System.out.println(Sum(a,b));
+        System.out.println(Minus(a,b));
+    }
+
+    public static String Sum(String a, String b){
+        int point = 0;
+        int val = 0;
+        String result = "";
+        for(int i = a.length()-1; i >= 0; i--){
+            val = Int(a.charAt(i)) + Int(b.charAt(i)) + point;
+            if(point != 0)
+                point = 0;
+            if(val >= 10){
+                point = 1;
+                val %= 10;
+                result = String.valueOf(val) + result;
+                continue;
+            }
+            result = String.valueOf(val) + result;
+        }
+        if(point != 0)
+            result = "1" + result;
+        return result;
+    }
+
+    public static String Minus(String a, String b){
+        int point = 0;
+        String result = "";
+        int val = 0;
+        int check = 0;
+        for(int i = 0; i <= a.length()-1; i++){
+            if(Int(a.charAt(i)) < Int(b.charAt(i))){
+                check = -1;
+                break;
+            }
+            if(Int(a.charAt(i)) > Int(b.charAt(i))){
+                check = 1;
+                break;
+            }
+        }
+        String c = "";
+        if(check == -1){
+            c = a;
+            a = b;
+            b = c;
+        }
+        for(int i = a.length()-1; i >= 0; i--){
+            val = Int(a.charAt(i)) + 10 - Int(b.charAt(i)) - point;
+            if(point != 0)
+                point = 0;
+            if(val < 10)
+                point++;
+            if(i == 0 && val % 10 == 0)
+                break;
+            result = String.valueOf(val % 10) + result;
+        }
+        if(check == -1)
+            return '-' + result;
+        return result;
+    }
+
+    public static String Time(String a, String b){
+        return "";
     }
 }
